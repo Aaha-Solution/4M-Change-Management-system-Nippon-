25469346acc0d1d02261615037eb92e660019893
Iyyanarsuriya <suriyaiyyanar10@gmail.com>
2026-06-23 16:30:58 +0530

    feat: create axios instance with JWT authentication interceptor and GET request deduplication

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `http://${window.location.hostname}:5001/api`,
+  baseURL: `https://nippon-project-final.onrender.com/api`,
   headers: {
     'Content-Type': 'application/json',
   },
