5e823508b0a405f626405fee786b6c2b6bd8d989
Iyyanarsuriya <suriyaiyyanar10@gmail.com>
2026-06-23 18:04:53 +0530

    final fix.

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -9,25 +9,11 @@ const pool = mysql.createPool({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: Number(process.env.MYSQLPORT),
-
   waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0,
   connectTimeout: 60000,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0
+  enableKeepAlive: true
 });
-
-(async () => {
-  try {
-    const conn = await pool.getConnection();
-    const [rows] = await conn.query('SELECT 1 AS test');
-    console.log('✅ Connected to MySQL:', rows);
-    conn.release();
-  } catch (error) {
-    console.error('❌ Error connecting to MySQL database');
-    console.error(error);
-  }
-})();
 
 export default pool;
